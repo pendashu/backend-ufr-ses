7b671d97af631e73ec6bfeca40247bbcb0aa1a41
ndeyependadiop <ndeyependad529@gmail.com>
2025-12-17 15:58:05 +0000

    Ajout dashboard server

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -41,9 +41,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-const express = require('express');
-const router = express.Router();
-
 router.get('/', (req, res) => {
   res.json({ message: "✅ Dashboard OK", status: 200 });
 });
